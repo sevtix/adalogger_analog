0659d937fd351d19d7e22a28954c77434f4c7ea5
sevtixdev <sevtixdev@gmail.com>
2023-05-07 13:17:03 +0200

    Update adalogger_analog.ino

--- adalogger_analog.ino
+++ adalogger_analog.ino
@@ -3,43 +3,18 @@
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
-// Set the pins used
-#define cardSelect 4
-
 File logfile;
 Adafruit_ADS1115 ads1115;
 
-// blink out an error code
-void error(uint8_t errno) {
-  while (1) {
-    uint8_t i;
-    for (i = 0; i < errno; i++) {
-      digitalWrite(13, HIGH);
-      delay(100);
-      digitalWrite(13, LOW);
-      delay(100);
-    }
-    for (i = errno; i < 10; i++) {
-      delay(200);
-    }
-  }
-}
-
-// This line is not needed if you have Adafruit SAMD board package 1.6.2+
-//   #define Serial SerialUSB
-
 void setup() {
   Serial.begin(115200);
-  randomSeed(128);
-  
   ads1115.begin();
 
   pinMode(13, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(13, OUTPUT);
 
   // see if the card is present and can be initialized:
-  if (!SD.begin(cardSelect)) {
+  if (!SD.begin(4)) {
     Serial.println("Card init. failed!");
     error(2);
   }
@@ -54,40 +29,70 @@ void setup() {
     }
   }
 
+  // open file in write mode
   logfile = SD.open(filename, FILE_WRITE);
   if (!logfile) {
-    Serial.print("Couldnt create ");
-    Serial.println(filename);
     error(3);
   }
-  Serial.print("Writing to ");
-  Serial.println(filename);
 
+  // header line for csv files
   logfile.println("Time, ADC, Volts");
 }
 
-long LoggerTimestamp = 0L;  // ms
-int LoggerInterval = 1;    //ms
+// blink out an error code
+void error(uint8_t c) {
+  while (true) {
+    uint8_t i;
+    for (i = 0; i < c; i++) {
+      digitalWrite(13, HIGH);
+      delay(100);
+      digitalWrite(13, LOW);
+      delay(100);
+    }
+    for (i = c; i < 10; i++) {
+      delay(200);
+    }
+  }
+}
 
+
+long LoggerLastTimestamp = 0L;  // ms
 long LoggerFlushTimestamp = 0L;  // ms
 int LoggerFlushInterval = 1000;  // ms;
 
 void loop() {
   long currentMillis = millis();
-  if (LoggerTimestamp <= currentMillis) {
+
+  // prevents multiple datapoints in a milisecond
+  if (LoggerLastTimestamp <= currentMillis) {
+
+    // green led on
     digitalWrite(8, HIGH);
+
+    // read ads1115 adc
     int16_t adc0 = ads1115.readADC_SingleEnded(0);
+
+    // convert to volt
     float volts0 = ads1115.computeVolts(adc0);
+
+    // log data
     logfile.print(currentMillis);
     logfile.print(",");
     logfile.print(adc0);
     logfile.print(",");
     logfile.println(volts0);
+
+    // flush if next timestamp reached
     if (LoggerFlushTimestamp <= currentMillis) {
+      // save log to file
       logfile.flush();
+      // set next flush timestamp
       LoggerFlushTimestamp = millis() + LoggerFlushInterval;
     }
+
+    // green led off
     digitalWrite(8, LOW);
-    LoggerTimestamp = millis() + LoggerInterval;
+
+    LoggerLastTimestamp = millis();
   }
 }
